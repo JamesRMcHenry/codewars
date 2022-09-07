6f79a820e7d1a75d3a5db413ac1df3c91e4ae75d
JamesRMcHenry <100225606+JamesRMcHenry@users.noreply.github.com>
2022-09-07 19:55:24 -0400

    first codewar challenge

---
+++ filter-array/test.js
@@ -0,0 +1,12 @@
+const numberOne = 1
+const numberTwo = 15
+
+function convertNumberToString(number) {
+    return number.toString()
+}
+
+const numberOneString = convertNumberToString(numberOne)
+const numberTwoString = convertNumberToString(numberTwo)
+
+console.log(typeof numberOneString)
+console.log(typeof numberTwoString)
