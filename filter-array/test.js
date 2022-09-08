bc88a27670cff464dbd991fe7e99f60468c58d08
JamesRMcHenry <100225606+JamesRMcHenry@users.noreply.github.com>
2022-09-07 20:03:45 -0400

    Added another test'

--- filter-array/test.js
+++ filter-array/test.js
@@ -1,5 +1,6 @@
 const numberOne = 1
 const numberTwo = 15
+const numberThree = 24
 
 function convertNumberToString(number) {
     return number.toString()
@@ -7,6 +8,8 @@ function convertNumberToString(number) {
 
 const numberOneString = convertNumberToString(numberOne)
 const numberTwoString = convertNumberToString(numberTwo)
+const numberThreeString = convertNumberToString(numberThree)
 
 console.log(typeof numberOneString)
 console.log(typeof numberTwoString)
+console.log(typeof numberThreeString)
